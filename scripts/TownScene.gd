65862146eaa995b6b9e24dcf301703a1102509dd
TheLawn <g_gazinur_g98@mail.ru>
2019-09-28 11:27:54 +0300

    added town map

--- scripts/TownScene.gd
+++ scripts/TownScene.gd
@@ -1,31 +1,117 @@
 extends Node2D
 var navbarShown=false
-var menuBtnImg=Image.new()
-var backBtnImg=Image.new()
+
+#var evt_index=-1
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	menuBtnImg.load("res://assets/menu.png")
-	backBtnImg.load("res://assets/back.jpg")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			if(!navbarShown):
+				$Tile1.visible=true
+				$Tile2.visible=true
+				$Tile3.visible=true
+				$Tile4.visible=true
+				$Tile5.visible=true
+				$Tile6.visible=true
+			$EventNotification.visible=false
+			
 func _on_TextureButton_pressed():
-	var texture=ImageTexture.new()
 	if(!navbarShown):
+		$Tile1.visible=false
+		$Tile2.visible=false
+		$Tile3.visible=false
+		$Tile4.visible=false
+		$Tile5.visible=false
+		$Tile6.visible=false
+		$TextureButton2.visible=true
+		$TextureButton.visible=false
 		$AnimationPlayer.play("NavbarShow")
 		navbarShown=true
-		texture.create_from_image(backBtnImg)
 	else:
+		$Tile1.visible=true
+		$Tile2.visible=true
+		$Tile3.visible=true
+		$Tile4.visible=true
+		$Tile5.visible=true
+		$Tile6.visible=true
+		$TextureButton2.visible=false
+		$TextureButton.visible=true
 		$AnimationPlayer.play("NavbarHide")
 		navbarShown=false
-		texture.create_from_image(menuBtnImg)
-	$TextureButton.texture_normal=texture
+	pass # Replace with function body.
+
+
+func _on_Tile1_pressed():
+	$Tile1.visible=false
+	$Tile2.visible=false
+	$Tile3.visible=false
+	$Tile4.visible=false
+	$Tile5.visible=false
+	$Tile6.visible=false
+	$EventNotification.visible=true
+	pass # Replace with function body.
+
+
+func _on_Tile2_pressed():
+	$Tile1.visible=false
+	$Tile2.visible=false
+	$Tile3.visible=false
+	$Tile4.visible=false
+	$Tile5.visible=false
+	$Tile6.visible=false
+	$EventNotification.visible=true
+	pass # Replace with function body.
+
+
+func _on_Tile3_pressed():
+	$Tile1.visible=false
+	$Tile2.visible=false
+	$Tile3.visible=false
+	$Tile4.visible=false
+	$Tile5.visible=false
+	$Tile6.visible=false
+	$EventNotification.visible=true
+	pass # Replace with function body.
+
+
+func _on_Tile4_pressed():
+	$Tile1.visible=false
+	$Tile2.visible=false
+	$Tile3.visible=false
+	$Tile4.visible=false
+	$Tile5.visible=false
+	$Tile6.visible=false
+	$EventNotification.visible=true
+	pass # Replace with function body.
+
+
+func _on_Tile5_pressed():
+	$Tile1.visible=false
+	$Tile2.visible=false
+	$Tile3.visible=false
+	$Tile4.visible=false
+	$Tile5.visible=false
+	$Tile6.visible=false
+	$EventNotification.visible=true
+	pass # Replace with function body.
+
+
+func _on_Tile6_pressed():
+	$Tile1.visible=false
+	$Tile2.visible=false
+	$Tile3.visible=false
+	$Tile4.visible=false
+	$Tile5.visible=false
+	$Tile6.visible=false
+	$EventNotification.visible=true
 	pass # Replace with function body.
